9d57e757edd893a85b2d55b6e6566f6e792d46ba
楚传麒 <1614168079@qq.com>
2023-09-19 22:26:56 +0800

    提交个人主页代码

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,46 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Introduction</template>
+    天蝎座，MBIT是ENFJ，喜欢打篮球和出去玩
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>School</template>
+
+    深圳大学
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Likes</template>
+    打篮球、健身、骑行、火锅
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Support Me</template>
+
+    如果想和我交朋友，请加我微信：15814435272，也可以通过微信转账的方式
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+  </WelcomeItem>
+</template>
